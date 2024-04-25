d623a5e21b775dd31105f70d60c3c57af401878d
Igor Todorovski <itodorov@ca.ibm.com>
2024-04-24 21:29:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/cppcheckport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/cppcheckport/actions/workflows/bump.yml)
+
+# cppcheck
+
+static analysis of C/C++ code
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cppcheckport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cppcheckport.git'), string(name: 'PORT_DESCRIPTION', value: 'static analysis of C/C++ code' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
